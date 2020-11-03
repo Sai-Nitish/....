584db4d309867c932a75cab2f30c1a46ea3c5306
Chavva Bala Meghanath Reddy <54951156+meghanath2052@users.noreply.github.com>
2020-11-03 13:09:46 +0530

    Update Assignment_4_CommandLineArguments.c

--- Assignment_4_CommandLineArguments.c
+++ Assignment_4_CommandLineArguments.c
@@ -1,34 +1,51 @@
 #include <stdio.h>
-int main(int argc, char *argv[])
+#include <stdlib.h>
+
+int main(int argc,char* argv[])
 {
- FILE *fp,*ft;
- char ch;
- fp=fopen(argv[1],"r");
- ft=fopen(argv[2],"w");
- if(argc==2)//For reading just one file
- {
-   if ( fp )
-   {
-     printf("File contents:\n");
-       while ( (ch = fgetc(fp)) != EOF )
-          {
-             printf("%c",ch);
-           }
-   }
-   else
-   {
-     printf("File error");
-   }
- }
- else
- {
-  while(ft)//For copying files
-  {
-   ch=fgetc(fp);
-   if (feof(fp)) break;
-   fputc(ch,ft);
-  }
-printf("Files Copied Successfully");
- }
-return 0;
+    FILE *fptr1,*fptr2;
+    char s,buffer[20];
+    if(argc>=2)
+    {
+        printf("Program name: %s\n\n",argv[0]);
+        printf("Arguments: ");
+        for(int i=1;i<argc;i++)
+        {
+            printf("%s ",argv[i]);
+        }
+        printf("\n");
+        fptr1=fopen(argv[1],"r");//file1 in read mode
+        if(fptr1==NULL)
+        {
+            printf("\nCannot open file %s \n",argv[1]);
+            exit(0);
+        }
+        fptr2=fopen(argv[2],"w");//file2 in write mode
+        if(fptr1==NULL)
+        {
+            printf("\nCannot open file %s \n",argv[2]);
+            exit(0);
+        }
+        printf("\nThe file1 contains: ");
+        while(fgets(buffer, sizeof(buffer),fptr1)!=NULL)
+        {
+            fputs(buffer,stdout);
+            fputs(buffer,fptr2);
+        }
+        printf("\n\nContents copied to %s\n",argv[2]);
+        fclose(fptr1);
+        fclose(fptr2);
+        fptr2=fopen(argv[2],"r");//file2 in read mode
+        printf("\nfile2 now contents: ");
+        while((s=fgetc(fptr2))!=EOF)
+        {
+            printf("%c",s);
+        }
+        fclose(fptr2);
+        printf("\n");
+        return 0;
+    }
+    else
+    { printf("Not enough arguments"); }
+    return 0;
 }
